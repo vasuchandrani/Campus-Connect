87f0c978953958bd510560ba1aa7d7218f2d80cc
Vatsal Chandrani <vasuchandrani1118@gmail.com>
2026-03-14 07:31:04 +0530

    add production frontend url to CORS

--- campusconnect-backend/src/main/java/com/campusconnect/campusconnectbackend/security/config/CorsConfig.java
+++ campusconnect-backend/src/main/java/com/campusconnect/campusconnectbackend/security/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://campus-connect-frontend-2ch7.onrender.com"
+                "https://campus-connect-frontend-2ch7.onrender.com",
+                "https://campus-connect-seven-alpha.vercel.app"
         ));
 
         // Allowed HTTP Methods
