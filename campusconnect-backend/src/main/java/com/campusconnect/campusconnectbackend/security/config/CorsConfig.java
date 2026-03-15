5348540b2aacc2c8061e1aabd937efc2e62055d0
Ronak Gondaliya <gondaliyaronak78@gmail.com>
2026-03-15 18:33:29 +0530

    CORS config

--- campusconnect-backend/src/main/java/com/campusconnect/campusconnectbackend/security/config/CorsConfig.java
+++ campusconnect-backend/src/main/java/com/campusconnect/campusconnectbackend/security/config/CorsConfig.java
@@ -20,8 +20,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://campus-connect-frontend-2ch7.onrender.com",
-                "https://campus-connect-seven-alpha.vercel.app"
+                "https://campus-connect-seven-alpha.vercel.app",
+                "https://campus-conect.xyz"
         ));
 
         // Allowed HTTP Methods
